1d66ebee03637b82f96c21faecd224d2a74f1211
veenlorivz <veenlorivz194@gmail.com>
2022-04-07 09:10:33 +0700

    Add Command To use BASE_URL For Development

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "https://sitp-monitoring.herokuapp.com/api";
 
+// BASE URL DEV
+// const BASE_URL = "https://localhost:5000/api";
+
 export const userRequest = axios.create({
   baseURL: BASE_URL,
 });
