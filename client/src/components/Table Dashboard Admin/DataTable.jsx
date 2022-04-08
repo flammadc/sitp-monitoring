7cef108f363ca18965302634d87f94c8b5da0d87
veenlorivz <veenlorivz194@gmail.com>
2022-04-08 09:54:56 +0700

    Set initial loader

--- client/src/components/Table Dashboard Admin/DataTable.jsx
+++ client/src/components/Table Dashboard Admin/DataTable.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment";
+import Loader from "react-js-loader";
 import { userRequest } from "../../requestMethods";
 import { CgProfile } from "react-icons/cg";
 
 const DataTable = ({ a }) => {
   const [user, setUser] = useState();
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const getUser = async () => {
+      setLoading(true);
       try {
         const res = await userRequest.get("users/" + a.id_user);
         setUser(res.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setLoading(false);
       }
     };
     getUser();
-  });
+  }, [a.id_user]);
 
   return (
     <tr className="dashboard-body-tr text-center">
       <td className="flex flex-row items-center">
-        {user?.profilePic.length ? (
+        {loading ? (
+          <Loader
+            type="spinner-default"
+            bgColor={"#0D8BFF"}
+            color={"#0D8BFF"}
+            size={40}
+          />
+        ) : user?.profilePic.length ? (
           <img
             src={user?.profilePic[0].url}
             className="w-8 h-8 mr-5 object-cover rounded-full"
@@ -29,6 +40,7 @@ const DataTable = ({ a }) => {
         ) : (
           <CgProfile className="w-8 h-8 mr-5" />
         )}
+
         <div className="">{user?.nama}</div>
       </td>
       <td>{a.judul}</td>
