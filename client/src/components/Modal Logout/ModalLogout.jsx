03f518885157b079ca8e5e25de437960ee752d32
veenlorivz <veenlorivz194@gmail.com>
2022-04-08 09:50:31 +0700

    Mobile view for logout modal

--- client/src/components/Modal Logout/ModalLogout.jsx
+++ client/src/components/Modal Logout/ModalLogout.jsx
@@ -19,25 +19,29 @@ const ModalLogout = () => {
 
   return (
     modal && (
-      <div className="scroll">
-        <div className="fixed inset-0 bg-overlay-color z-50 duration-300"></div>
-        <div className="w-1/3 h-auto fixed translate-x-50- translate-y-50- top-1/2 left-1/2 bg-white z-50 shadow-lg font-Lato px-12 py-9 rounded-md duration-300">
-          <div className="text-2xl text-center mb-2">Keluarkan Akun</div>
-          <div className="text-lg text-center mb-7">
-            Apakah Anda Yaking Keluarkan Akun
+      <div className="w-screen h-screen flex items-center justify-center bg-transparent fixed z-40">
+        <div className="flex items-center justify-center fixed inset-0 bg-overlay-color z-50"></div>
+        <div className="w-[40vh] sm:w-[70vh] fixed  z-50 min-h-52  grid grid-cols-12 bg-white  shadow-lg font-Lato px-12 py-9 rounded-md ">
+          <div className="text-2xl text-center mb-2 col-span-12 font-bold">
+            Keluarkan Akun
           </div>
-          <button
-            className="w-40 h-12 bg-white border-2 rounded-md border-border-main-color mr-5"
-            onClick={() => dispatch(logoutCanceled())}
-          >
-            Batal
-          </button>
-          <button
-            className="w-40 h-12 bg-red-400  rounded-md  text-white"
-            onClick={handleLogout}
-          >
-            Ya, Keluar
-          </button>
+          <div className="text-sm md:text-lg text-center mb-7 col-span-12">
+            Anda Yakin Keluarkan Akun ?
+          </div>
+          <div className="col-span-12 flex flex-row items-center">
+            <button
+              className="h-12 min-w-max w-52 mr-5 bg-white border-2 rounded-md border-border-main-color"
+              onClick={() => dispatch(logoutCanceled())}
+            >
+              Batal
+            </button>
+            <button
+              className="h-12 min-w-max w-52 bg-red-400  rounded-md  text-white"
+              onClick={handleLogout}
+            >
+              Ya, Keluar
+            </button>
+          </div>
         </div>
       </div>
     )
