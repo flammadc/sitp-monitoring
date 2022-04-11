fc8ec450ca1bdfb5493363b697c2d92edeee45a3
veenlorivz <veenlorivz194@gmail.com>
2022-04-11 11:20:51 +0700

    Centering buttons and fixed size of container

--- client/src/components/Modal Logout/ModalLogout.jsx
+++ client/src/components/Modal Logout/ModalLogout.jsx
@@ -21,14 +21,14 @@ const ModalLogout = () => {
     modal && (
       <div className="w-screen h-screen flex items-center justify-center bg-transparent fixed z-40">
         <div className="flex items-center justify-center fixed inset-0 bg-overlay-color z-50"></div>
-        <div className="w-[80vw] sm:w-[70vh] fixed  z-50 min-h-52  grid grid-cols-12 bg-white  shadow-lg font-Lato px-12 py-9 rounded-md ">
+        <div className="w-[80vw] lg:w-[50vw] fixed  z-50 min-h-52  grid grid-cols-12 bg-white  shadow-lg font-Lato px-12 py-9 rounded-md ">
           <div className="text-2xl text-center mb-2 col-span-12 font-bold">
             Keluarkan Akun
           </div>
           <div className="text-sm md:text-lg text-center mb-7 col-span-12">
             Anda Yakin Keluarkan Akun ?
           </div>
-          <div className="col-span-12 flex flex-row items-center">
+          <div className="col-span-12 flex flex-row items-center justify-center">
             <button
               className="h-12 min-w-max w-52 mr-5 bg-white border-2 rounded-md border-border-main-color"
               onClick={() => dispatch(logoutCanceled())}
